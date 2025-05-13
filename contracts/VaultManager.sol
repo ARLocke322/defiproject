9ea65d7018cad969a0674e801a04c8b8e0ffbbd1
Alex Locke <alexander.locke@kcl.ac.uk>
2025-05-13 17:52:12 +0100

    refactor: add reentrancy protection and simplify error handling in VaultManager

--- contracts/VaultManager.sol
+++ contracts/VaultManager.sol
@@ -1,9 +1,8 @@
 pragma solidity ^0.8.28;
 import { USDToken } from "./USDToken.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
-
-
-contract VaultManager {
+contract VaultManager is ReentrancyGuard {
     struct Vault {
     uint256 collateralETH; // ETH locked by user (in wei)
     uint256 debtMyUSD;     // MyUSD minted by user (18 decimals)
@@ -12,22 +11,13 @@ contract VaultManager {
     uint256 public constant bonusPercent = 105;
 
     mapping(address => Vault) public vaults;
+
     event CollateralDeposited(address indexed _to, uint256 amount);
     event CollateralWithdrawn(address indexed _from, uint256 amount);
     event CollateralLiquidated(address indexed user, address indexed liquidator, uint256 repayAmount, uint256 ethReward);
 
     event USDTokenMinted(address indexed _from, uint256 amount);
     event USDTokenBurned(address indexed _from, uint256 amount);
-
-    error NotEnoughCollateral(address caller);
-    error NotEnoughDebt(address caller);
-    error InsufficientTokenBalance(address caller);
-
-    error WithdrawFailed(address caller);
-    error LiquidateFailed(address caller);
-    error TokenTransferFailed(address caller);
-    error EthTransferFailed(address caller);
-
 
     USDToken public immutable usdToken;
 
@@ -42,48 +32,40 @@ contract VaultManager {
 
     function mint(uint256 amount) public {
         require(amount > 0, "Amount must be > 0");
-        if ((vaults[msg.sender].collateralETH * ethPrice / 1e18) >= ((vaults[msg.sender].debtMyUSD + amount) * 150 / 100)) {
-            usdToken.mint(msg.sender, amount);
-            vaults[msg.sender].debtMyUSD += amount;
-            emit USDTokenMinted(msg.sender, amount);
-        } else {
-            revert NotEnoughCollateral(msg.sender);
-        }
+        require((vaults[msg.sender].collateralETH * ethPrice / 1e18) >= ((vaults[msg.sender].debtMyUSD + amount) * 150 / 100), "Not enough collateral");
+
+        usdToken.mint(msg.sender, amount);
+        vaults[msg.sender].debtMyUSD += amount;
+        emit USDTokenMinted(msg.sender, amount);
+
     }
 
     function burn(uint256 amount) public {
-        if (usdToken.balanceOf(msg.sender) < amount) {
-            revert InsufficientTokenBalance(msg.sender);
-        }
-
         require(amount > 0, "Amount must be > 0");
-        if (vaults[msg.sender].debtMyUSD >= amount) {
-            usdToken.burn(msg.sender, amount);
-            vaults[msg.sender].debtMyUSD -= amount;
-            emit USDTokenBurned(msg.sender, amount);
-        } else {
-            revert NotEnoughDebt(msg.sender);
-        }
+        require((usdToken.balanceOf(msg.sender) >= amount), "Insufficient token balance");
+        require(vaults[msg.sender].debtMyUSD >= amount, "Not enough debt");
+   
+        usdToken.burn(msg.sender, amount);
+        vaults[msg.sender].debtMyUSD -= amount;
+        emit USDTokenBurned(msg.sender, amount);
+
     }
 
-    function withdrawCollateral(uint256 amount) public {
+    function withdrawCollateral(uint256 amount) public nonReentrant {
         require(amount > 0, "Amount must be > 0");
         require(vaults[msg.sender].collateralETH >= amount, "Not enough collateral to withdraw");
+        require(((vaults[msg.sender].collateralETH - amount) * ethPrice / 1e18) >= (vaults[msg.sender].debtMyUSD * 150 / 100), "Withdrawing would cause debt to be undercollateralised");
 
-        if (((vaults[msg.sender].collateralETH - amount) * ethPrice / 1e18) >= (vaults[msg.sender].debtMyUSD * 150 / 100)) {
-            (bool success, ) = msg.sender.call{value: amount}("");
-            if (!success) {
-                revert WithdrawFailed(msg.sender);
-            } else {
-                vaults[msg.sender].collateralETH -= amount;
-                emit CollateralWithdrawn(msg.sender, amount);
-            }
-        } else {
-            revert NotEnoughCollateral(msg.sender);
-        }
+        (bool success, ) = msg.sender.call{value: amount}("");
+        require(success, "ETH transfer failed");
+
+        vaults[msg.sender].collateralETH -= amount;
+        emit CollateralWithdrawn(msg.sender, amount);
+    
+
     }
 
-    function liquidate(address user, uint256 repayAmount) public payable { // repayAmount in USDTKN
+    function liquidate(address user, uint256 repayAmount) public nonReentrant { // repayAmount in USDTKN
         uint256 ethReward = repayAmount * bonusPercent * 1e18 / ethPrice / 100;
 
         require(((vaults[user].collateralETH * ethPrice / 1e18) < (vaults[user].debtMyUSD * 150 / 100)), "Vault not undercollateralised");
@@ -91,19 +73,17 @@ contract VaultManager {
         require(repayAmount > 0, "Amount must be > 0");
         require(repayAmount <= vaults[user].debtMyUSD, "Cannot repay more than debt");
 
-
         usdToken.transferFrom(msg.sender, address(this), repayAmount);
     
         (bool ethTransferSuccess, ) = msg.sender.call{value: ethReward}("");
-        if (!ethTransferSuccess) {
-            revert EthTransferFailed(msg.sender);
-        } else {
-            usdToken.burn(address(this), repayAmount);
-            vaults[user].collateralETH -= ethReward;
-            vaults[user].debtMyUSD -= repayAmount;
-            emit CollateralLiquidated(user, msg.sender, repayAmount, ethReward);
+        require(ethTransferSuccess, "ETH transfer failed");
 
-        }  
+        usdToken.burn(address(this), repayAmount);
+        vaults[user].collateralETH -= ethReward;
+        vaults[user].debtMyUSD -= repayAmount;
+        emit CollateralLiquidated(user, msg.sender, repayAmount, ethReward);
+
+   
     }
 
     function getVault(address user) external view returns (uint256 collateralETH, uint256 debtMyUSD) {
