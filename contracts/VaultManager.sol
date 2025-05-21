4dee29f28eff663325a827b790505e4964a4333f
Alex Locke <alexlocke@Alexs-Mac-mini.local>
2025-05-21 17:00:48 +0100

    add zero liquidation loan functionality with CR enforcement and toggle events

--- contracts/VaultManager.sol
+++ contracts/VaultManager.sol
@@ -6,6 +6,7 @@ contract VaultManager is ReentrancyGuard {
     struct Vault {
     uint256 collateralETH; // ETH locked by user (in wei)
     uint256 debtMyUSD;     // MyUSD minted by user (18 decimals)
+    bool zeroLiquidation; // is zero liquidation toggled
     }
     uint256 public constant ethPrice = 2000e18;
     uint256 public constant bonusPercent = 105;
@@ -19,7 +20,13 @@ contract VaultManager is ReentrancyGuard {
     event USDTokenMinted(address indexed _from, uint256 amount);
     event USDTokenBurned(address indexed _from, uint256 amount);
 
+    event ZeroLiquidationEnabled(address indexed user);
+    event ZeroLiquidationDisabled(address indexed user);
+
     USDToken public immutable usdToken;
+    uint256 public constant STANDARD_COLLATERAL_RATIO = 150e16; // 150%
+    uint256 public constant ZERO_LIQUIDATION_COLLATERAL_RATIO = 250e16; // 250%
+
 
     constructor(address _usdToken) {
         usdToken = USDToken(_usdToken);
@@ -32,47 +39,63 @@ contract VaultManager is ReentrancyGuard {
 
     function mint(uint256 amount) public {
         require(amount > 0, "Amount must be > 0");
-        require((vaults[msg.sender].collateralETH * ethPrice / 1e18) >= ((vaults[msg.sender].debtMyUSD + amount) * 150 / 100), "Not enough collateral");
+
+        Vault storage vault = vaults[msg.sender];
+        uint256 collateralValue = vault.collateralETH * ethPrice / 1e18;
+        uint256 newDebt = vault.debtMyUSD + amount;
+        uint256 collateralRatio = vault.zeroLiquidation ? ZERO_LIQUIDATION_COLLATERAL_RATIO : STANDARD_COLLATERAL_RATIO;
+        require((collateralValue * 1e18) / newDebt >= collateralRatio, "Not enough collateral"); // Ensure collateral ratio (scaled by 1e18) meets required minimum
 
         usdToken.mint(msg.sender, amount);
-        vaults[msg.sender].debtMyUSD += amount;
+        vault.debtMyUSD += amount;
         emit USDTokenMinted(msg.sender, amount);
-
     }
 
     function burn(uint256 amount) public {
+        Vault storage vault = vaults[msg.sender];
         require(amount > 0, "Amount must be > 0");
-        require(vaults[msg.sender].debtMyUSD >= amount, "Not enough debt");
+        require(vault.debtMyUSD >= amount, "Not enough debt");
         require((usdToken.balanceOf(msg.sender) >= amount), "Insufficient token balance");
         
    
         usdToken.burn(msg.sender, amount);
-        vaults[msg.sender].debtMyUSD -= amount;
+        vault.debtMyUSD -= amount;
         emit USDTokenBurned(msg.sender, amount);
-
     }
 
     function withdrawCollateral(uint256 amount) public nonReentrant {
+        Vault storage vault = vaults[msg.sender];
         require(amount > 0, "Amount must be > 0");
-        require(vaults[msg.sender].collateralETH >= amount, "Not enough collateral to withdraw");
-        require(((vaults[msg.sender].collateralETH - amount) * ethPrice / 1e18) >= (vaults[msg.sender].debtMyUSD * 150 / 100), "Withdrawing would cause debt to be undercollateralised");
+        require(vault.collateralETH >= amount, "Not enough collateral to withdraw");
+
+        uint256 newCollateral = vault.collateralETH - amount;
+        uint256 newCollateralValue = newCollateral * ethPrice / 1e18;
+        uint256 collateralRatio = vault.zeroLiquidation ? ZERO_LIQUIDATION_COLLATERAL_RATIO : STANDARD_COLLATERAL_RATIO;
+        
+        if (vault.debtMyUSD > 0) {
+            require((newCollateralValue * 1e18) / vault.debtMyUSD >= collateralRatio, "Withdrawing would cause debt to be undercollateralised");
+        }
+
+        vault.collateralETH -= amount;
 
         (bool success, ) = msg.sender.call{value: amount}("");
         require(success, "ETH transfer failed");
 
-        vaults[msg.sender].collateralETH -= amount;
-        emit CollateralWithdrawn(msg.sender, amount);
-    
-
+        emit CollateralWithdrawn(msg.sender, amount);  
     }
 
     function liquidate(address user, uint256 repayAmount) public nonReentrant { // repayAmount in USDTKN
-        uint256 ethReward = repayAmount * bonusPercent * 1e18 / ethPrice / 100;
+        Vault storage vault = vaults[user];
+        require(!vault.zeroLiquidation, "Zero Liquidation vault cannot be liquidated");
 
-        require(((vaults[user].collateralETH * ethPrice / 1e18) < (vaults[user].debtMyUSD * 150 / 100)), "Vault not undercollateralised");
-        require(ethReward <= vaults[user].collateralETH, "Not enough ETH in vault");
+        uint256 ethReward = repayAmount * bonusPercent * 1e18 / ethPrice / 100;
+        uint256 collateralValue = vault.collateralETH * ethPrice / 1e18;
+
+        require(vault.debtMyUSD > 0, "Vault has no debt");
+        require(((collateralValue * 1e18) / vault.debtMyUSD < STANDARD_COLLATERAL_RATIO), "Vault not undercollateralised");
+        require(ethReward <= vault.collateralETH, "Not enough ETH in vault");
         require(repayAmount > 0, "Amount must be > 0");
-        require(repayAmount <= vaults[user].debtMyUSD, "Cannot repay more than debt");
+        require(repayAmount <= vault.debtMyUSD, "Cannot repay more than debt");
 
         usdToken.transferFrom(msg.sender, address(this), repayAmount);
     
@@ -80,8 +103,8 @@ contract VaultManager is ReentrancyGuard {
         require(ethTransferSuccess, "ETH transfer failed");
 
         usdToken.burn(address(this), repayAmount);
-        vaults[user].collateralETH -= ethReward;
-        vaults[user].debtMyUSD -= repayAmount;
+        vault.collateralETH -= ethReward;
+        vault.debtMyUSD -= repayAmount;
         emit CollateralLiquidated(user, msg.sender, repayAmount, ethReward);
 
    
@@ -92,16 +115,39 @@ contract VaultManager is ReentrancyGuard {
         return (vault.collateralETH, vault.debtMyUSD);
     }       
 
-    function getCollateralRatio(address user) external view returns (uint256 ratio) {
+    function getCollateralRatio(address user) public view returns (uint256 ratio) {
         // returns current collateral ratio of vault
         Vault memory vault = vaults[user];
         if (vault.debtMyUSD == 0) return type(uint256).max;
         uint256 collateralUSD = vault.collateralETH * ethPrice / 1e18;
         return collateralUSD * 1e18 / vault.debtMyUSD;
     }
+
+    function enableZeroLiquidation() public {
+        Vault storage vault = vaults[msg.sender];
+        uint256 collateralValue = vault.collateralETH * ethPrice / 1e18;
+        require(!vault.zeroLiquidation, "Vault already has Zero Liquidation enabled");
+        require(vault.debtMyUSD > 0, "Vault has no debt");
+        require((collateralValue * 1e18) / vault.debtMyUSD >= ZERO_LIQUIDATION_COLLATERAL_RATIO, "Not enough collateral");
+        vault.zeroLiquidation = true;
+        emit ZeroLiquidationEnabled(msg.sender);
+    }
+
+    function disableZeroLiquidation() public {
+        Vault storage vault = vaults[msg.sender];
+        uint256 collateralValue = vault.collateralETH * ethPrice / 1e18;
+        require(vault.zeroLiquidation, "Vault does not have Zero Liquidation enabled");
+        require(vault.debtMyUSD > 0, "Vault has no debt");
+        require((collateralValue * 1e18) / vault.debtMyUSD >= STANDARD_COLLATERAL_RATIO, "Not enough collateral");
+        vault.zeroLiquidation = false;
+        emit ZeroLiquidationDisabled(msg.sender);
+    }
+   
+
+
 // remove before deployment
     function test_setVault(address user, uint256 collateralETH, uint256 debtMyUSD) external {
-        vaults[user] = Vault(collateralETH, debtMyUSD);
+        vaults[user] = Vault(collateralETH, debtMyUSD, false);
     }
 
     function test_liquidateWithoutRewardCheck(address user, uint256 repayAmount) public nonReentrant {
